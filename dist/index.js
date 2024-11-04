4ed0c7572b3ec1886a330eae449530c3b2caeb94
Lautaro Montaña <joselautarom@gmail.com>
2024-11-04 12:20:34 -0300

    Clase 1

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let mensaje = "Hola Mundo";
+mensaje = "chanchito feliz";
+mensaje = "chau mundo";
+console.log(mensaje);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let mensaje = "Hola Mundo";
+
+mensaje = "chanchito feliz";
+
+mensaje = "chau mundo";
+console.log(mensaje)
+
