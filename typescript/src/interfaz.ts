8c923299d96506f83ff55807539c7cd8e627be6b
Lautaro Montaña <joselautarom@gmail.com>
2024-11-07 06:33:25 -0300

    Explicacion interfaz

--- typescript/src/interfaz.ts
+++ typescript/src/interfaz.ts
@@ -9,3 +9,26 @@ const multip: Operacion =(a, b) => a * b;
 console.log(sum(5, 3));
 console.log(multip(5, 3));
 
+// _______________________________________________
+
+function resta1(a: number, b: number){
+    return a - b;
+}
+
+const operaciones: Operacion[] = [sum, multip, resta1];
+
+operaciones.forEach((operacion) => console.log(operacion(10, 5)));
+
+
+// _______________________________________________
+
+// Tipazo avanzado con parametros opciones y predeterminados
+
+interface Calculadora {
+    (a: number, b: number, c?: number): number;
+}
+
+const calcular: Calculadora = (a, b, c = 0) => a + b + c;
+
+console.log(calcular(4, 5));
+console.log(calcular(4, 5, 6));
